80ddea26e33ab12cfcd1771d0d3fa1ad3c6ee3fc
Tim Schwartz <tim@metaverse.systems>
2022-09-01 11:22:02 -0500

    gdt_create_entry

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -1,6 +1,7 @@
 #include "gdt.h"
+#include <string.h>
 
-gdt_desc get_gdt()
+gdt_desc gdt_get()
 {
     gdt_desc gdt;
     __asm__ __volatile__ (
@@ -11,4 +12,55 @@ gdt_desc get_gdt()
     );
 
     return gdt;
+}
+
+void gdt_set(gdt_desc *gdt)
+{
+    __asm__ __volatile__ (
+    "lgdt %0"
+    :
+    : "m" (gdt)
+    : "memory"
+    );
+}
+
+gdt_desc gdt_init(size_t count, void *(*malloc)(uint64_t))
+{
+    size_t length = count * 8;
+
+    gdt_desc gdt = { .limit = length - 1 };
+
+    gdt.base = (uint64_t)malloc(length);
+
+    memset((uint64_t *)(gdt.base), 0, length);
+
+    return gdt;
+}
+
+gdt_entry gdt_entry_create(uint32_t base, uint32_t limit, uint8_t access, uint8_t flags)
+{
+    gdt_entry entry;
+
+    uint64_t base1 = base >> 24;
+    uint64_t base2 = (base >> 16) & 0xFF;
+    uint64_t base3 = base & 0xFFFF;
+
+    uint64_t limit1 = (limit >> 16) & 0xF;
+    uint64_t limit2 = limit & 0xFFFF;
+
+    entry = base1 << 56;
+    entry |= ((uint64_t)flags & 0xF) << 52;
+    entry |= limit1 << 48;
+    entry |= (uint64_t)access << 40;
+    entry |= base2 << 32;
+    entry |= base3 << 16;
+    entry |= limit2;
+
+    return entry;
+}
+
+void gdt_entry_add(gdt_desc *desc, uint8_t index, gdt_entry entry)
+{
+    gdt_entry *ptr = (gdt_entry *)desc->base;
+    ptr[index] = entry;
 }
