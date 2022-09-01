8c0d02619a552fee0c6107abf46532b98c752ba5
Tim Schwartz <tim@metaverse.systems>
2022-09-01 11:23:32 -0500

    vmtest

--- vm-test.sh
+++ vm-test.sh
@@ -5,4 +5,4 @@ source envvars.sh
 cp efi-stage/bootx64.efi ${LOOP_TARGET}/EFI/boot/
 cp efi-stage/*.sfn ${LOOP_TARGET}/EFI/boot/
 cp kernel/kernel ${LOOP_TARGET}/EFI/boot
-qemu-system-x86_64 -m 4096 -cpu qemu64 -smp 4 -bios /usr/share/qemu/OVMF.fd -drive file=${DEV_ROOT}/images/disk.img,if=ide,format=raw -rtc base=localtime
+qemu-system-x86_64 -M q35 -m 4096 -cpu qemu64 -smp 4 -bios /usr/share/qemu/OVMF.fd -drive file=${DEV_ROOT}/images/disk.img,if=virtio,format=raw
